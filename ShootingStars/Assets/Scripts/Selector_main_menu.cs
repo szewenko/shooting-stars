c2a474223593891cff07428454a944835fc821b5
Kamil Koziol <kamilkoziol@gmail.com>
2017-12-24 16:22:56 +0100

    dzwiek wyboru pola w menu

--- ShootingStars/Assets/Scripts/Selector_main_menu.cs
+++ ShootingStars/Assets/Scripts/Selector_main_menu.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class Selector_main_menu : MonoBehaviour {
 
 	public int mode = 1;
+    public AudioSource[] aSources;
     private float[] position_mode_1 = { (float)0.05, (float)0.156, (float)0.4 };
     private float[] position_mode_2 = { (float)0.05, (float)0.058, (float)0.4 };
     private float[] position_mode_3 = { (float)0.05, (float)-0.03, (float)0.4 };
@@ -22,14 +23,17 @@ public class Selector_main_menu : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			mode--;
 			if (mode <1) mode = 4;
-		}
+            GetComponent<AudioSource>().Play();
+        }
 
 		if (Input.GetKeyDown (KeyCode.DownArrow)){
 			mode++;
 			if (mode > 4) mode = 1;
-			}
+            GetComponent<AudioSource>().Play();
+        }
 
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)){
+            
             switch (mode)
             {
                 case 1:
